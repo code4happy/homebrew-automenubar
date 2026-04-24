5f841177706036f37f3af0c006c9b5c18daf8d2b
code4happy <jayatwork@foxmail.com>
2026-04-24 10:59:07 +0800

    add cask rb file

---
+++ Casks/automenubar.rb
@@ -0,0 +1,26 @@
+cask "automenubar" do
+  version "1.0.0"
+  sha256 "0adadbd0d5252f6b7da162f64b56f6a4c381bb589ca63fe090f91ae434b24b24"
+
+  url "https://github.com//AutoMenuBar/releases/download/v#{version}/AutoMenuBar-#{version}-macOS.zip"
+  name "AutoMenuBar"
+  desc "macOS menu bar icon management tool with dual-icon positioning system"
+  homepage "https://github.com/code4happy/AutoMenuBar"
+
+  auto_updates true
+
+  app "AutoMenuBar.app"
+
+  zap trash: [
+    "~/Library/Application Support/AutoMenuBar",
+    "~/Library/Caches/com.yourcompany.AutoMenuBar",
+    "~/Library/Preferences/com.yourcompany.AutoMenuBar.plist",
+  ]
+
+  caveats do
+    <<~EOS
+      AutoMenuBar 需要辅助功能权限来管理菜单栏图标。
+      首次运行时，请前往 系统设置 > 隐私与安全性 > 辅助功能 > 添加 AutoMenuBar
+    EOS
+  end
+end
